b8e5c992844ac0395ba14d603169d7d50d34313b
wangjiangtao <249805310@qq.com>
2019-11-19 21:03:56 +0800

    余额记录  创建时间查询

--- src/main/java/com/zc/bean/ZcBalance.java
+++ src/main/java/com/zc/bean/ZcBalance.java
@@ -1,4 +1,5 @@
 package com.zc.bean;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +8,8 @@ import java.io.Serializable;
 import java.util.*;
 
 /**
-*
-*  @author author
-*/
+ * @author author
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,50 +19,54 @@ public class ZcBalance implements Serializable {
 
 
     /**
-    * 主键
-    * 
-    * isNullAble:0
-    */
+     * 主键
+     * <p>
+     * isNullAble:0
+     */
     private Long id;
 
     /**
      * 用户Id
-     * */
+     */
     private Long userId;
 
     /**
-    * 
-    * isNullAble:1
-    */
+     * isNullAble:1
+     */
     private String code;
 
     /**
-    * 类型 1上级充值 2下级扣费  3后台充值套餐  4总后台扣除  5自动充值
-    * isNullAble:1
-    */
+     * 类型 1上级充值 2下级扣费  3后台充值套餐  4总后台扣除  5自动充值
+     * isNullAble:1
+     */
     private Integer type;
 
     /**
-    * 金额 都是乘100之后
-    * isNullAble:1
-    */
+     * 金额 都是乘100之后
+     * isNullAble:1
+     */
     private Long amount;
 
     /**
-    * 变更后余额 都是乘100之后
-    * isNullAble:1
-    */
+     * 变更后余额 都是乘100之后
+     * isNullAble:1
+     */
     private Long balanceAfterChange;
 
     /**
-    * 创建记录时间
-    * isNullAble:1
-    */
+     * 创建记录时间
+     * isNullAble:1
+     */
     private Date createTime;
 
     /**
-    * 备注
-    * isNullAble:1
-    */
+     * 备注
+     * isNullAble:1
+     */
     private String remarks;
+    /**
+     * =====================辅助字段===========================
+     */
+    private String createStartTime;
+    private String creteEndTime;
 }
