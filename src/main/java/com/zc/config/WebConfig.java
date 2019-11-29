30f9b1be7e824754531864ff818ea885501dcdb5
wangjiangtao <249805310@qq.com>
2019-11-29 18:00:38 +0800

    整合页面到项目中

--- src/main/java/com/zc/config/WebConfig.java
+++ src/main/java/com/zc/config/WebConfig.java
@@ -1,17 +1,15 @@
-/*
 package com.zc.config;
 
 import com.zc.interceptor.ActionInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-*/
 /**
  * @author wangjiangtao
- *//*
-
+ */
 @Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
     @Bean
@@ -19,18 +17,15 @@ public class WebConfig extends WebMvcConfigurationSupport {
         return new ActionInterceptor();
     }
 
-    */
-/**
+    /**
      * 添加登录拦截
-     *//*
-
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         super.addInterceptors(registry);
         registry.addInterceptor(actionInterceptor())
                 .addPathPatterns("/**")
                 .excludePathPatterns("/login/**")
-                .excludePathPatterns("/index");
+                .excludePathPatterns("/index/**","/static/**","/templates/**");
     }
 }
-*/
